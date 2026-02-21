d7a9f3cb0afa77db4ced8fe83ed9b9df8d6f00c7
HannaJoline <jolinebergsjo@gmail.com>
2026-02-21 11:36:35 +0100

    Initial Commit

---
+++ ChildCardKeeper/ChildCardKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChildCardKeeperApp.swift
+//  ChildCardKeeper
+//
+//  Created by Joline Bergsjö on 2026-02-21.
+//
+
+import SwiftUI
+
+@main
+struct ChildCardKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
